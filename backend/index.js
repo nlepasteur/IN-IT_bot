55446e9797ef53933eaf168812b7a8dd96865008
Nicolas Le Pasteur <nicolas.lepasteur@gmail.com>
2020-05-28 19:33:26 +0200

    heroku issue

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-require('./routes/dialogFlowRoutes')(app);
+require('./server/routes/dialogFlowRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
